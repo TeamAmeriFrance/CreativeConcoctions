1261bd9e7bc992267ad7fe92a231a915150ff6b7
Tombenpotter <thomas@benguigui.fr>
2015-03-19 19:32:16 +0100

    Brew of Brawn!

---
+++ src/main/java/amerifrance/concoctions/concoctions/ConcoctionStrength.java
@@ -0,0 +1,36 @@
+package amerifrance.concoctions.concoctions;
+
+import amerifrance.concoctions.api.Concoction;
+import amerifrance.concoctions.api.IConcoctionContext;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.util.StatCollector;
+
+import java.awt.*;
+import java.util.UUID;
+
+public class ConcoctionStrength extends Concoction {
+
+    public UUID uuid;
+
+    public ConcoctionStrength() {
+        super(StatCollector.translateToLocal("concoction.strength"), 60, Color.GREEN);
+        uuid = UUID.randomUUID();
+    }
+
+    @Override
+    public void onEffectAdded(EntityLivingBase livingBase, IConcoctionContext ctx) {
+        AttributeModifier r = livingBase.getEntityAttribute(SharedMonsterAttributes.attackDamage).getModifier(uuid);
+        if (r != null) livingBase.getEntityAttribute(SharedMonsterAttributes.attackDamage).removeModifier(r);
+
+        AttributeModifier modifier = new AttributeModifier(uuid, name, 1.0D * ctx.getConcoctionLevel(), 2);
+        livingBase.getEntityAttribute(SharedMonsterAttributes.attackDamage).applyModifier(modifier);
+    }
+
+    @Override
+    public void onEffectRemoved(EntityLivingBase livingBase, IConcoctionContext ctx) {
+        AttributeModifier r = livingBase.getEntityAttribute(SharedMonsterAttributes.attackDamage).getModifier(uuid);
+        if (r != null) livingBase.getEntityAttribute(SharedMonsterAttributes.attackDamage).removeModifier(r);
+    }
+}
--- src/main/java/amerifrance/concoctions/concoctions/ModConcoctions.java
+++ src/main/java/amerifrance/concoctions/concoctions/ModConcoctions.java
@@ -6,11 +6,15 @@ import amerifrance.concoctions.api.ConcoctionsRegistry;
 public class ModConcoctions {
 
     public static Concoction speed;
+    public static Concoction strength;
     public static Concoction hellEyes;
 
     public static void registerConcoctions() {
         speed = new ConcoctionSpeed();
         ConcoctionsRegistry.registerConcoction(speed, "ConcoctionSpeed");
+
+        strength = new ConcoctionStrength();
+        ConcoctionsRegistry.registerConcoction(strength, "ConcoctionStrength");
 
         hellEyes = new ConcoctionHellEyes();
         ConcoctionsRegistry.registerConcoction(hellEyes, "HellEyes");
