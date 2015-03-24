319d84ad86427d124f04b9fbf8eeab13f4d3cd88
Tombenpotter <thomas@benguigui.fr>
2015-03-24 19:19:45 +0100

    It would seem getComponents() exists :p

--- src/main/java/amerifrance/concoctions/concoctions/compound/ConcoctionSayanSerum.java
+++ src/main/java/amerifrance/concoctions/concoctions/compound/ConcoctionSayanSerum.java
@@ -11,15 +11,13 @@ import java.awt.*;
 
 public class ConcoctionSayanSerum extends Concoction {
 
-    public Concoction[] parents = {ModConcoctions.strength, ModConcoctions.speed, ModConcoctions.resistance, ModConcoctions.regeneration, ModConcoctions.jumpBoost, ModConcoctions.slowness, ModConcoctions.mineSlow, ModConcoctions.hunger};
-
     public ConcoctionSayanSerum() {
         super("concoction.sayan.serum", 20, Color.YELLOW, ConcoctionType.NEUTRAL, ModConcoctions.strength, ModConcoctions.speed, ModConcoctions.resistance, ModConcoctions.regeneration, ModConcoctions.jumpBoost, ModConcoctions.slowness, ModConcoctions.mineSlow, ModConcoctions.hunger);
     }
 
     @Override
     public void onEffectAdded(EntityLivingBase livingBase, IConcoctionContext ctx) {
-        for (Concoction c : parents) {
+        for (Concoction c : getComponents()) {
             if (c.getConcotionType() == ConcoctionType.GOOD || c.getConcotionType() == ConcoctionType.NEUTRAL)
                 c.onEffectAdded(livingBase, ctx);
         }
@@ -27,7 +25,7 @@ public class ConcoctionSayanSerum extends Concoction {
 
     @Override
     public void updateEffect(EntityLivingBase livingBase, IConcoctionContext ctx) {
-        for (Concoction c : parents) {
+        for (Concoction c : getComponents()) {
             if (c.getConcotionType() == ConcoctionType.GOOD || c.getConcotionType() == ConcoctionType.NEUTRAL)
                 c.updateEffect(livingBase, ctx);
         }
@@ -35,7 +33,7 @@ public class ConcoctionSayanSerum extends Concoction {
 
     @Override
     public void onEffectRemoved(EntityLivingBase livingBase, IConcoctionContext ctx) {
-        for (Concoction c : parents) {
+        for (Concoction c : getComponents()) {
             if (c.getConcotionType() == ConcoctionType.GOOD || c.getConcotionType() == ConcoctionType.NEUTRAL)
                 c.onEffectRemoved(livingBase, ctx);
             else ConcoctionsHelper.addConcoction(livingBase, c, ctx.getConcoctionLevel(), ctx.getInitialDuration());
