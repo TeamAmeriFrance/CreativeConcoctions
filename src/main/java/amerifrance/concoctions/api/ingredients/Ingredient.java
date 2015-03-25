67fb6fef47049255b2fa85b52b4f76e0d855b15c
Tombenpotter <thomas@benguigui.fr>
2015-03-25 22:55:52 +0100

    Added a way to get a list of properties

--- src/main/java/amerifrance/concoctions/api/ingredients/Ingredient.java
+++ src/main/java/amerifrance/concoctions/api/ingredients/Ingredient.java
@@ -1,16 +1,23 @@
 package amerifrance.concoctions.api.ingredients;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Ingredient {
 
     public final IngredientType ingredientType;
     public final float stability;
     public final int potency;
-    public final IngredientProperties[] properties;
+    private final IngredientProperties[] properties;
 
     public Ingredient(IngredientType ingredientType, float stability, int potency, IngredientProperties... properties) {
         this.ingredientType = ingredientType;
         this.stability = stability;
         this.potency = potency;
         this.properties = properties;
+    }
+
+    public List<IngredientProperties> getProperties() {
+        return Arrays.asList(properties);
     }
 }
