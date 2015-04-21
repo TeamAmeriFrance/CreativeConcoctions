3525f45453982b224001a0646c4188e454b05c7b
Tombenpotter <thomas@benguigui.fr>
2015-04-21 18:24:27 +0200

    gg github

--- src/main/java/amerifrance/concoctions/guide/PageIngredientProperty.java
+++ src/main/java/amerifrance/concoctions/guide/PageIngredientProperty.java
@@ -10,6 +10,8 @@ import amerifrance.guideapi.api.abstraction.EntryAbstract;
 import amerifrance.guideapi.api.base.Book;
 import amerifrance.guideapi.api.base.PageBase;
 import amerifrance.guideapi.gui.GuiBase;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.util.EnumChatFormatting;
@@ -23,6 +25,7 @@ public class PageIngredientProperty extends PageBase {
     }
 
     @Override
+    @SideOnly(Side.CLIENT)
     public void draw(Book book, CategoryAbstract category, EntryAbstract entry, int guiLeft, int guiTop, int mouseX, int mouseY, GuiBase guiBase, FontRenderer fontRenderer) {
         String text = "Known ingredients: \n ";
 
