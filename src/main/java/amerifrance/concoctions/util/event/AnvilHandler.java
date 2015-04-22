55bca45c8ffd20a24e2273db4f9f6ffc406e279b
Tombenpotter <thomas@benguigui.fr>
2015-04-22 19:55:19 +0200

    Works with creative ingredients too now

--- src/main/java/amerifrance/concoctions/util/event/AnvilHandler.java
+++ src/main/java/amerifrance/concoctions/util/event/AnvilHandler.java
@@ -1,9 +1,11 @@
 package amerifrance.concoctions.util.event;
 
+import amerifrance.concoctions.api.ingredients.IPropertiesContainer;
 import amerifrance.concoctions.api.ingredients.IngredientProperty;
 import amerifrance.concoctions.concoctions.basic.ConcoctionKnowledge;
 import amerifrance.concoctions.items.ItemBottledIngredient;
 import amerifrance.concoctions.items.ItemConcoction;
+import amerifrance.concoctions.items.ItemCreativeIngredient;
 import amerifrance.concoctions.items.ItemKnowledgePhial;
 import amerifrance.concoctions.registry.ItemsRegistry;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
@@ -20,11 +22,12 @@ public class AnvilHandler {
         if (left == null || right == null) return;
 
         if (left.getItem() instanceof ItemConcoction && ItemConcoction.getConcoction(left) != null && ItemConcoction.getConcoction(left) instanceof ConcoctionKnowledge) {
-            if (right.getItem() instanceof ItemBottledIngredient) {
-                ItemBottledIngredient bottledIngredient = (ItemBottledIngredient) right.getItem();
-                if (bottledIngredient.getIngredientProperties(right).size() == 1) {
-                    IngredientProperty ingredientProperty = bottledIngredient.getIngredientProperties(right).get(0);
-                    int potency = bottledIngredient.getIngredientPotency(right);
+            if (right.getItem() instanceof ItemBottledIngredient || right.getItem() instanceof ItemCreativeIngredient) {
+                IPropertiesContainer propertiesContainer = (IPropertiesContainer) right.getItem();
+                
+                if (propertiesContainer.getIngredientProperties(right).size() == 1) {
+                    IngredientProperty ingredientProperty = propertiesContainer.getIngredientProperties(right).get(0);
+                    int potency = propertiesContainer.getIngredientPotency(right);
 
                     ItemStack output = new ItemStack(ItemsRegistry.knowledgePhial);
                     ItemKnowledgePhial.setProperty(output, ingredientProperty);
