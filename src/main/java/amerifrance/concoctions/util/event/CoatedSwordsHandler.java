d21511549ac2bb7994814096e2ba15d9be0039ce
Tombenpotter <thomas@benguigui.fr>
2015-05-01 13:06:32 +0200

    Correct tooltips :3

--- src/main/java/amerifrance/concoctions/util/event/CoatedSwordsHandler.java
+++ src/main/java/amerifrance/concoctions/util/event/CoatedSwordsHandler.java
@@ -2,12 +2,12 @@ package amerifrance.concoctions.util.event;
 
 import amerifrance.concoctions.api.CreativeConcoctionsAPI;
 import amerifrance.concoctions.api.concoctions.ConcoctionsHelper;
-import cpw.mods.fml.common.eventhandler.EventPriority;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemSword;
+import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.util.StatCollector;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
@@ -29,17 +29,20 @@ public class CoatedSwordsHandler {
         }
     }
 
-    @SubscribeEvent(priority = EventPriority.HIGHEST)
+    @SubscribeEvent
     public void onCoatedTooltip(ItemTooltipEvent event) {
         if (event.itemStack != null && event.itemStack.getItem() instanceof ItemSword) {
-            if (!GuiScreen.isShiftKeyDown()) return;
             ItemStack stack = event.itemStack;
 
             if (CreativeConcoctionsAPI.getConcoction(stack) != null) {
-                event.toolTip.add(CreativeConcoctionsAPI.getConcoction(stack).getConcotionType().prefix + CreativeConcoctionsAPI.getConcoction(stack).name);
-                event.toolTip.add(String.format(StatCollector.translateToLocal("gui.text.level"), CreativeConcoctionsAPI.getLevel(stack)));
-                if (!CreativeConcoctionsAPI.getConcoction(stack).isConcoctionInstant())
-                    event.toolTip.add(String.format(StatCollector.translateToLocal("gui.text.duration"), (double) CreativeConcoctionsAPI.getDuration(stack) / 20 + "s"));
+                event.toolTip.set(0, stack.getDisplayName() + " (" + CreativeConcoctionsAPI.getConcoction(stack).getConcotionType().prefix + CreativeConcoctionsAPI.getConcoction(stack).name + EnumChatFormatting.RESET + ")");
+
+                if (GuiScreen.isShiftKeyDown()) {
+                    event.toolTip.add(1, CreativeConcoctionsAPI.getConcoction(stack).getConcotionType().prefix + CreativeConcoctionsAPI.getConcoction(stack).name);
+                    event.toolTip.add(2, String.format(StatCollector.translateToLocal("gui.text.level"), CreativeConcoctionsAPI.getLevel(stack)));
+                    if (!CreativeConcoctionsAPI.getConcoction(stack).isConcoctionInstant())
+                        event.toolTip.add(3, String.format(StatCollector.translateToLocal("gui.text.duration"), (double) CreativeConcoctionsAPI.getDuration(stack) / 20 + "s"));
+                }
             }
         }
     }
